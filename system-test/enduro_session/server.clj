198c6bd3e74f1b24bb9717d8ed0257fb4925e181
Simon Lundmark <simon.lundmark@gmail.com>
2014-05-01 18:55:04 +0200

    Use an ordinary in-memory session store

--- system-test/enduro_session/server.clj
+++ system-test/enduro_session/server.clj
@@ -1,14 +1,24 @@
 (ns enduro-session.server
   (:gen-class :main true)
-  (:use ring.adapter.jetty))
+  (:use clojure.pprint
+        ring.util.response
+        ring.adapter.jetty
+        ring.middleware.session))
+
+(defn log
+  [& messages]
+  (print "server.clj> ")
+  (apply println messages))
 
 (defn handler
   [request]
-  {:status 200
-   :headers {"Content-Type" "text/html"}
-   :body "Hello World"})
+  (log "I got a" (:request-method request) "request")
+  (if (= (:request-method request) :post)
+    (-> (redirect-after-post "/")
+        (assoc :session {:posted-data (slurp (:body request))}))
+    (response (str (:session request)))))
 
 (defn -main
   [port]
-  (println "Starting system test web server on port" port)
-  (run-jetty handler {:port (Integer. port)}))
+  (log "Starting system test web server on port" port)
+  (run-jetty (wrap-session handler) {:port (Integer. port)}))
